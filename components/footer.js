3720f6fbbe17b94435605a3ed837296832e9ca2d
info3170sbartnicki <s_bartnicki@fanshaweonline.ca>
2022-10-14 21:56:28 -0400

    test

--- components/footer.js
+++ components/footer.js
@@ -1,12 +1,12 @@
-let component = {
-  title: 'Footer',
-  description: 'Footer',
-  html: '',
-  htmlSource: './htmlComponents/footer.html',
-};
+let footer = {
+    title: 'Footer',
+    description: "Section at the bottom of the page with more information and contact details.",
+    html: "",
+    htmlSource: "./htmlComponents/footer.html",
+    };
 
-fetch(component.htmlSource)
-  .then((response) => response.text())
-  .then((text) => (component.html = text));
-
-export default component;
+fetch(footer.htmlSource)
+.then((response) => response.text())
+.then((text) => (footer.html = text));
+  
+export default footer;
