5e57392acde87b88d6d27cdbaa76ef6604c737f8
LMauger <113475945+LMauger@users.noreply.github.com>
2022-10-18 19:01:22 -0400

    added page layout files/page layout added to index (#10)
    * added page layout files/page layout added to index
    
    * fixed pageLayout.js
    
    * Removed inline css, fixed css footer class
    
    * formatting changes
    
    * formatting changes
    
    * fixed page layout component
    
    * links in footer
    
    Co-authored-by: Pavel Nagornyuk <p.nagornyuk@cleack.com>
    Co-authored-by: info3170sbartnicki <s_bartnicki@fanshaweonline.ca>

--- components/pageLayouts.js
+++ components/pageLayouts.js
@@ -1,6 +1,12 @@
-let alerts = {
-  title: 'Page Layouts',
-  description: 'Layouts description',
-};
+let pageLayout = {
+  title: 'Page Layout',
+  description: "Overview of the general dashboard layout for web and mobile, as well as chart layouts for web and mobile.",
+  html: "",
+  htmlSource: "./htmlComponents/pageLayout.html",
+  };
 
-export default alerts;
+fetch(pageLayout.htmlSource)
+.then((response) => response.text())
+.then((text) => (pageLayout.html = text));
+
+export default pageLayout;
