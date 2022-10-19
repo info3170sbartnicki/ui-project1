bfaf1d86e369b3a539ea62494649aebb3c68ed80
LMauger <113475945+LMauger@users.noreply.github.com>
2022-10-18 22:02:49 -0400

    Added sizes to page layout and fixed footer note (#22)

--- components/footer.js
+++ components/footer.js
@@ -1,6 +1,6 @@
 let footer = {
     title: 'Footer',
-    description: "Section at the bottom of the page with more information and contact details.",
+    description: "Section at the bottom of the page with more information and contact details. Just add updated links to the instances of a href for functionality.",
     html: "",
     htmlSource: "./htmlComponents/footer.html",
     };
