581cd661549bc0c8e984b7cf9077ab6aea0dff03
info3170anantdeepdhingra <113368808+info3170anantdeepdhingra@users.noreply.github.com>
2022-12-14 17:04:07 -0500

    Update main.js

--- main.js
+++ main.js
@@ -57,7 +57,7 @@ listTotal.forEach((item, index) => {
 // 2. Add a name of chart wrapper to chartWrappers array (it will create div with such ID)
 // 3. Call the function that draws your chart.
 let dashboard = document.querySelector('#dashboard');
-let chartsWrappers = ['area_chart', 'histogram', 'gauge-chart', 'arc-chart', 'progress']; //add the name of your chart container to this array
+let chartsWrappers = ['area_chart', 'histogram', 'gauge-chart', 'arc-chart', 'radial-chart']; //add the name of your chart container to this array
 
 dashboard.addEventListener('click', () => {
   componentBox.innerHTML = ``;
