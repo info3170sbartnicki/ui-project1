b572d31ee393c251dfdd8d1e8833daac6263f04d
info3170anantdeepdhingra <113368808+info3170anantdeepdhingra@users.noreply.github.com>
2022-12-15 09:29:44 -0500

    Anantdeep chart7/8 (#28)
    * Add files via upload
    
    * Update chart7.html
    
    Updated donut chart
    
    * Chart 8 (gauge chart)
    
    Shows distance travelled
    
    * Add files via upload
    
    * Update index.html
    
    * Update main.js
    
    * JS File added
    
    * Delete gauge.html
    
    * Delete chart7.html
    
    * Update main.js
    
    * Update gaugeChart.js
    
    * Update gaugeChart.js
    
    * Update gaugeChart.js
    
    colour change
    
    * Update gaugeChart.js
    
    added title
    
    * Update gaugeChart.js
    
    * Update gaugeChart.js
    
    * Add files via upload
    
    * Add files via upload
    
    * Update main.js
    
    * Update and rename progressbarChart.js to arcChart.js
    
    * Update main.js
    
    * Update and rename progressbarChart.css to arcChart.css
    
    * Update main.js
    
    * Update arcChart.js
    
    * Update index.html
    
    * Update arcChart.js
    
    * Update arcChart.css
    
    * Update arcChart.js
    
    * Update arcChart.js
    
    * Update arcChart.js
    
    * Update arcChart.js
    
    * Update arcChart.js
    
    * Update arcChart.css
    
    * Update arcChart.js
    
    * Update arcChart.js
    
    * Update arcChart.js
    
    * Update arcChart.js
    
    * Update gaugeChart.js
    
    * Update gaugeChart.js
    
    * Update gaugeChart.js
    
    * Update gaugeChart.js
    
    * Add files via upload
    
    * Update radial.js
    
    * Add files via upload
    
    * Update main.js
    
    * Update main.js
    
    * Update radial.js
    
    * Update main.js
    
    * Update radial.js
    
    * Update radial.css
    
    * Update radial.css
    
    * Update radial.css
    
    * Update radial.js
    
    * Update radial.js
    
    * Update radial.js
    
    * Update radial.js
    
    * Update radial.js
    
    * Update radial.js
    
    * Update radial.js
    
    * Update radial.js
    
    * Update radial.js
    
    * Update radial.js
    
    * Update radial.js
    
    * Delete arcChart.js
    
    * Delete arcChart.css
    
    * Update main.js
    
    * Update radial.js
    
    * Update radial.js
    
    * Update radial.js
    
    * Update radial.js
    
    * Update index.html
    
    * Update radial.js
    
    * Update radial.js
    
    * Update radial.js
    
    * Update radial.js
    
    * Update radial.js
    
    * Update radial.js
    
    Co-authored-by: info3170sbartnicki <113321843+info3170sbartnicki@users.noreply.github.com>

---
+++ charts/gaugeChart.js
@@ -0,0 +1,72 @@
+export default function gaugeChart() {
+    
+    // set the dimensions and margins of the graph
+    var width = 400;
+    var height = 100;
+    var margin = 20;
+
+    // The maximum value of the gauge
+    var maxValue = 150000;
+
+    // The initial value of the gauge
+    var initialValue = 0;
+
+    // The desired final value of the gauge
+    var finalValue = 108132;
+
+    //Retrieve Data from CSV
+    d3.csv("./charts/data/customer_satisfaction.csv").then( function(data) {
+      
+      //Find the sum of flight distance and save to a variable
+      var sum = d3.sum(data,function (d) { return d['Flight Distance']; })
+      console.log(sum)
+
+
+    // append the svg object to the body of the page
+    var svg = d3.select("#gauge-chart")
+      .append("svg")
+        .attr("width", width)
+        .attr("height", height)
+      .append("g")
+        .attr("transform", "translate(" + margin + "," + margin + ")");
+       
+
+    // Create a linear scale
+    var x = d3.scaleLinear()
+      .domain([0, maxValue])
+      .range([0, width - 2 * margin]);
+
+    // Draw the gauge
+    var gauge = svg.append("rect")
+      .attr("x", 0)
+      .attr("y", 0)
+      .attr("width", x(maxValue))
+      .attr("height", height - 2 * margin)
+      .style("fill", "#573280")
+
+    // Add a value indicator
+    var valueIndicator = svg.append("rect")
+      .attr("x", 0)
+      .attr("y", 0)
+      .attr("width", x(initialValue))
+      .attr("height", height - 2 * margin)
+      .style("fill", "#FFA306")
+
+    // Add a scale
+    var xAxis = d3.axisBottom()
+      .scale(x)
+
+    svg.append("g")
+      .attr("transform", "translate(0," + (height - 2 * margin) + ")")
+      .style('color', 'var(--light)') 
+      .call(xAxis);
+
+    // Animate the gauge
+    valueIndicator
+      .transition()
+      .duration(2000)
+      .attr("width", x(sum))
+
+    });
+
+}
--- main.js
+++ main.js
@@ -14,8 +14,13 @@ import footer from './components/footer.js';
 import dataVisualization from './components/dataVisualization.js';
 
 import histogram from './charts/histogramChart3.js';
+
+import gaugeChart from './charts/gaugeChart.js';
+import radialChart from './charts/radial.js';
+
 import stackedBarChart from './charts/chart9.js';
 import barChart from './charts/chart6.js';
+
 
 let componentBox = document.getElementById('componentBox'); // Inside of it we'll display current component
 let components = [
@@ -55,7 +60,10 @@ listTotal.forEach((item, index) => {
 // 2. Add a name of chart wrapper to chartWrappers array (it will create div with such ID)
 // 3. Call the function that draws your chart.
 let dashboard = document.querySelector('#dashboard');
-let chartsWrappers = ['histogram', 'stackedBarChart', 'barChart']; //add the name of your chart container to this array
+
+let chartsWrappers = ['histogram', 'gauge-chart', 'radial-chart', 'stackedBarChart', 'barChart']; //add the name of your chart container to this array
+
+
 
 dashboard.addEventListener('click', () => {
   componentBox.innerHTML = ``;
@@ -68,6 +76,11 @@ dashboard.addEventListener('click', () => {
 
   //call your chart function here
   histogram();
+
+  gaugeChart();
+  radialChart();
+
   stackedBarChart();
   barChart();
+
 });
