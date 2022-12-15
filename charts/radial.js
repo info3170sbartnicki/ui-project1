369aabedf49a970dacbbc6540508529e58a0c08c
info3170anantdeepdhingra <113368808+info3170anantdeepdhingra@users.noreply.github.com>
2022-12-15 00:08:32 -0500

    Update radial.js

--- charts/radial.js
+++ charts/radial.js
@@ -1,11 +1,22 @@
 export default function radialChart() {
-document.getElementById('radial-chart').innerHTML = '';
-//var wrapper = document.getElementById('progress');
-var start = 0;
-var end = parseFloat("40");
+//retrieve data
+d3.csv("./charts/data/customer_satisfaction.csv").then( function(data) {
+
+    
+    data.forEach(dataItem => {
+                if (dataItem['Customer Type'].toLowerCase() === 'loyal customer') {
+                    var countCustomers = (dataItem,function (d) { return d['Customer Type'].toLowerCase() === 'loyal customer'; }
+                    //console.log( countCustomers) //this outputs 40 values (therefore there are 40 loyal customers)
+                )} 
+                
+            })
+    
+var container = document.getElementById('radial-chart');
+var beginning = 0;
+var end = 40; // this is the value that the chart fills up to (amount of loyal customers calculated above)
 
 var colours = {
-  fill: '#' + "FFA306",
+   fill: '#' + "FFA306",
   track: '#' + "573280",
   text: '#' + "ffffff",
   stroke: '#' + "23022F",
@@ -18,40 +29,27 @@ var endAngle = Math.PI * 2;
 var formatText = d3.format('.0');
 var boxSize = radius * 2;
 var count = end;
-var progress = start;
-var step = end < start ? -0.01 : 0.01;
+var progress = beginning;
+var step = end < beginning ? -0.01 : 0.01;
 
 
-//retrieve data
-d3.csv("./charts/data/customer_satisfaction.csv").then( function(data) {
-
-    var sum = d3.count(data,function (d) { return d['Customer Type']; })
-      console.log(sum)
-
-      data.forEach(dataItem => {
-                if (dataItem['Customer Type'].toLowerCase() === 'loyal customer') {
-                    //var sum = d3.count(dataItem,function (d) { return d['Customer Type']; })
-                    console.log(dataItem)
-                } 
-            })
-
-//Define the circle
+//Create the circle
 var circle = d3.arc()
   .startAngle(0)
   .innerRadius(radius)
   .outerRadius(radius - border);
 
-//setup SVG wrapper
-var svg = d3.select('#radial-chart')
+//SVG container
+var svg = d3.select(container)
   .append('svg')
   .attr('width', boxSize)
   .attr('height', boxSize);
 
-// ADD Group container
+// Group container
 var g = svg.append('g')
   .attr('transform', 'translate(' + boxSize / 2 + ',' + boxSize / 2 + ')');
 
-//Setup track
+//Tracking 
 var track = g.append('g').attr('class', 'radial-progress');
 track.append('path')
   .attr('class', 'radial-progress__background')
@@ -60,7 +58,7 @@ track.append('path')
   .attr('stroke-width', strokeSpacing + 'px')
   .attr('d', circle.endAngle(endAngle));
 
-//Add colour fill
+//Adding colour to fill
 var value = track.append('path')
   .attr('class', 'radial-progress__value')
   .attr('fill', colours.fill)
@@ -69,7 +67,7 @@ var value = track.append('path')
 
 //Add text value
 var numberText = track.append('text')
-  .attr('class', 'radial-progress__text')
+  .attr('class', 'radial-progress')
   .attr('fill', colours.text)
   .attr('text-anchor', 'middle')
   .style("font-size", "50px")
@@ -79,7 +77,7 @@ function update(progress) {
   //update position of endAngle
   value.attr('d', circle.endAngle(endAngle * progress));
   //update text value
-  numberText.text(formatText(progress)* 100);
+  numberText.text(formatText(progress) * 100);
 } 
 
 (function iterate() {
