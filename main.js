bbe89e7284cc711ae5957293b709ea80170f23a9
LMauger <113475945+LMauger@users.noreply.github.com>
2022-12-15 09:22:24 -0500

    Charts9/6 (#32)
    * Added sizes to page layout and fixed footer note
    
    * chart9WIP
    
    * charts9/6complete
    
    * charts9/6final
    
    * Delete launch.json. This should be in gitignore
    
    * Deleted areaChart
    
    Co-authored-by: info3170sbartnicki <113321843+info3170sbartnicki@users.noreply.github.com>

--- main.js
+++ main.js
@@ -14,6 +14,8 @@ import footer from './components/footer.js';
 import dataVisualization from './components/dataVisualization.js';
 
 import histogram from './charts/histogramChart3.js';
+import stackedBarChart from './charts/chart9.js';
+import barChart from './charts/chart6.js';
 
 let componentBox = document.getElementById('componentBox'); // Inside of it we'll display current component
 let components = [
@@ -53,7 +55,7 @@ listTotal.forEach((item, index) => {
 // 2. Add a name of chart wrapper to chartWrappers array (it will create div with such ID)
 // 3. Call the function that draws your chart.
 let dashboard = document.querySelector('#dashboard');
-let chartsWrappers = ['area_chart', 'histogram']; //add the name of your chart container to this array
+let chartsWrappers = ['histogram', 'stackedBarChart', 'barChart']; //add the name of your chart container to this array
 
 dashboard.addEventListener('click', () => {
   componentBox.innerHTML = ``;
@@ -66,4 +68,6 @@ dashboard.addEventListener('click', () => {
 
   //call your chart function here
   histogram();
+  stackedBarChart();
+  barChart();
 });
